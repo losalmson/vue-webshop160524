e707869ec4110f99eb6b8f4ebbd95852ca64d629
losalmson <mialo.salmson@gmail.com>
2024-05-19 22:17:25 +0200

    created template layout for products with interpolation

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -11,6 +11,14 @@ defineProps<ProductProps>();
 </script>
 
 <template>
+    <article class="product">
+        <h3> {{ product.name }}</h3>
+        <div class="image-container">
+            <img :src="product.imageUrl" :alt="product.name"/>
+            <p> {{ product.description }}</p>
+            <span> {{ product.price }}</span>
+        </div>
+    </article>
 </template>
 
 <style scoped>
---
+++ src/models/IWebshopState.ts
@@ -0,0 +1,5 @@
+import { IProduct } from "./IProduct";
+
+export interface IWebshopState {
+    products: IProduct[];
+}
