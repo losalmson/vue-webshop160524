7d3e539317aeaee732abb1ccf21d9661f7306406
losalmson <mialo.salmson@gmail.com>
2024-05-19 22:03:10 +0200

    created interface for ProductProps

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { IProduct } from '../models/IProduct';
+
+interface ProductProps {
+    product: IProduct;
+}
+
+defineProps<ProductProps>();
+
 </script>
 
 <template>
